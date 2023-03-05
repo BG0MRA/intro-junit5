2341953a5c68cfe9e80ec3cd1e3942321333d1d4
Martin Angelov <martin.angelov02@gmail.com>
2023-03-05 14:34:27 +0200

    intro JUnit5

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,11 +1,15 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
 
     Greeting greeting;
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before All - called once!!!");
+    }
 
     @BeforeEach
     void setUp() {
@@ -22,5 +26,19 @@ class GreetingTest {
     @Test
     void testHelloWorld() {
         System.out.println(greeting.helloWorld("Martin"));
+    }
+    @Test
+    void testHelloWorld2() {
+        System.out.println(greeting.helloWorld("Sam"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After Each...");
+    }
+
+    @AfterAll
+    public static void AfterAll() {
+        System.out.println("After All - called once!!!");
     }
 }
